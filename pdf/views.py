3f2c60af85a1451f9ef6f0a491b7248c97ef3ec2
rajesh <sivarajesh.143@gmail.com>
2023-01-10 18:00:19 +0530

    my app first commit

---
+++ pdf/views.py
@@ -0,0 +1,12 @@
+from reportlab.pdfgen import canvas
+from django.http import HttpResponse
+
+def getpdf(request):
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="file.pdf"'
+    p = canvas.Canvas(response)
+    p.setFont("Times-Roman", 55)
+    p.drawString(100,700, "Hello, SivaRajesh.")
+    p.showPage()
+    p.save()
+    return response
